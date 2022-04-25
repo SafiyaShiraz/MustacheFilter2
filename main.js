b5933b29d1eafa63af958c52431d811f79b4cad3
SafiyaShiraz <93498306+SafiyaShiraz@users.noreply.github.com>
2022-04-24 21:59:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload()
+{
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+
+
+  function draw()
+  {
+
+  }
+
+  function take_snapshot()
+  {
+      save("MyFilterImage.png")
+  }
+
+  function modelLoaded()
+  {
+      console.log("Posenet Model Loaded.")
+  }
